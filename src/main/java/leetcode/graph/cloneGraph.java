de9b7bd04836a0112ac5cba831d1f5db10654214
Vickey <vickeysingh35@gmail.com>
2019-01-15 06:15:17 +0530

    Comment question part in clone graph solution

--- src/main/java/leetcode/graph/cloneGraph.java
+++ src/main/java/leetcode/graph/cloneGraph.java
@@ -1,8 +1,8 @@
-133. Clone Graph
+// 133. Clone Graph
 
-Given the head of a graph, return a deep copy (clone) of the graph. Each node in the graph contains a label (int) and
-a list (List[UndirectedGraphNode]) of its neighbors. There is an edge between the given node and each of the nodes in
-its neighbors.
+// Given the head of a graph, return a deep copy (clone) of the graph. Each node in the graph contains a label (int) and
+// a list (List[UndirectedGraphNode]) of its neighbors. There is an edge between the given node and each of the nodes in
+// its neighbors.
 
 import java.util.Map;
 
